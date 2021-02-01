482ea490ad8cfea39232455d35c10d6d04fd908c
Elvis Maradzike <maradzikee@ornl.gov>
2021-02-01 14:00:11 -0500

    Cleaned up syntax

--- tests/h4.sh
+++ tests/h4.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+HOME=/home/parallels
+OBJECT_FILE_LOCATION=$HOME/projects/my_cas-tn_branch/cas-tn
 
-g++-9 -g `/home/parallels/.exatn/bin/exatn-config --cxxflags` `/home/parallels/.exatn/bin/exatn-config --includes` -c h4.cpp -o h4.o 
-g++-9 h4.o /home/parallels/projects/my_cas-tn_branch/cas-tn/simulation.o `/home/parallels/.exatn/bin/exatn-config --libs` -o h4.x 
+g++-9 -g `$HOME/.exatn/bin/exatn-config --cxxflags` `$HOME/.exatn/bin/exatn-config --includes` -c h4.cpp -o h4.o 
+g++-9 h4.o $OBJECT_FILE_LOCATION/simulation.o `$HOME/.exatn/bin/exatn-config --libs` -o h4.x 
 
