8ca8365785498b711d7758b60b5ec9251780a849
Elvis Maradzike <maradzikee@ornl.gov>
2021-06-26 02:52:35 -0400

    Cleaned up source code and removed old data files

--- tests/h4_contiguous.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-HOME=/home/parallels
-g++-9 -g `$HOME/.exatn/bin/exatn-config --cxxflags` `$HOME/.exatn/bin/exatn-config --includes` -c h4_contiguous.cpp -o h4_contiguous.o 
-g++-9 h4_contiguous.o `$HOME/.exatn/bin/exatn-config --libs` -o h4_contiguous.x 
-
--- tests/h4_unfactorized.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-HOME=/home/parallels
-OBJECT_FILE_LOCATION=$HOME/projects/my_cas-tn_branch/cas-tn
-g++-9 -g `$HOME/.exatn/bin/exatn-config --cxxflags` `$HOME/.exatn/bin/exatn-config --includes` -c h4_unfactorized.cpp -o h4_unfactorized.o 
-g++-9 h4_unfactorized.o $OBJECT_FILE_LOCATION/simulation.o `$HOME/.exatn/bin/exatn-config --libs` -o h4_unfactorized.x 
-
