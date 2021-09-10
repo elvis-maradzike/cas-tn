5c0f21565051613b8ad731503ade7235297bfbd7
Elvis Maradzike <maradzikee@ornl.gov>
2021-09-10 17:13:48 -0400

    Initialized ordering projector tensor using functionality already built-in in ExaTN

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -68,31 +68,29 @@ void Simulation::markOptimizableTensors(){
 }
 
 bool Simulation::optimize(std::size_t num_states, double convergence_thresh){
-
-  auto ham = exatn::makeSharedTensorOperator("Hamiltonian");
-  //(anti)symmetrization 
-  auto success = ham->appendSymmetrizeComponent(hamiltonian_[0],{0,1},{2,3}, num_particles_, num_particles_,{1.0,0.0},true); assert(success);
-  success = ham->appendSymmetrizeComponent(hamiltonian_[1],{0},{1}, num_particles_, num_particles_,{1.0,0.0},true); assert(success);
-
+  
   //marking optimizable tensors
   markOptimizableTensors();
   
   //appending ordering projectors
   appendOrderingProjectors();
  
-  ket_ansatz_->rename("VectorExpansion"); 
-  //bra ansatz
+  ket_ansatz_->rename("KetExpansion"); 
   bra_ansatz_ = std::make_shared<exatn::TensorExpansion>(*ket_ansatz_);
-  bra_ansatz_->rename(ket_ansatz_->getName()+"Bra");
+  bra_ansatz_->rename("BraExpansion");
   bra_ansatz_->conjugate();
   
   //initializing optimizable tensors
   initWavefunctionAnsatz();
 
+  // hamiltonian operator
+  auto hamiltonian_operator = exatn::makeSharedTensorOperator("HamiltonianOperator");
+    //(anti)symmetrization 
+  auto success = hamiltonian_operator->appendSymmetrizeComponent(hamiltonian_[0],{0,1},{2,3}, num_particles_, num_particles_,{1.0,0.0},true); assert(success);
+  success = hamiltonian_operator->appendSymmetrizeComponent(hamiltonian_[1],{0},{1}, num_particles_, num_particles_,{1.0,0.0},true); assert(success);
   // setting up and calling the optimizer in ../src/exatn/..
   exatn::TensorNetworkOptimizer::resetDebugLevel(1);
-  exatn::TensorNetworkOptimizer optimizer(ham,ket_ansatz_,1e-4);
-  optimizer.resetTolerance(1e-4);
+  exatn::TensorNetworkOptimizer optimizer(hamiltonian_operator,ket_ansatz_,convergence_thresh_);
   optimizer.resetLearningRate(0.5);
   optimizer.resetMicroIterations(1);
   optimizer.resetDebugLevel(2);
@@ -110,33 +108,9 @@ bool Simulation::optimize(std::size_t num_states, double convergence_thresh){
 /** Appends two layers of ordering projectors to the wavefunction ansatz. **/
 void Simulation::appendOrderingProjectors(){
   const auto TENS_ELEM_TYPE = exatn::TensorElementType::REAL64;
-  const auto TENS_SHAPE_2INDEX = exatn::TensorShape{total_orbitals_, total_orbitals_};
-  const auto TENS_SHAPE_4INDEX = exatn::TensorShape{total_orbitals_,total_orbitals_,total_orbitals_,total_orbitals_};
-
-// array of elements to initialize tensors that comprise the ordering projectors
-  std::vector<double> orderingProjectorData(total_orbitals_*total_orbitals_*total_orbitals_*total_orbitals_,0.0);
-  for ( unsigned int i = 0; i < total_orbitals_; i++){
-    for ( unsigned int j = 0; j < total_orbitals_; j++){
-      for ( unsigned int k = 0; k < total_orbitals_; k++){
-        for ( unsigned int l = 0; l < total_orbitals_; l++){
-          if ( i < j){
-          orderingProjectorData[i*total_orbitals_*total_orbitals_*total_orbitals_
-                 +j*total_orbitals_*total_orbitals_
-                 +k*total_orbitals_+l] = double((i==k)*(j==l));
-          }else{
-          orderingProjectorData[i*total_orbitals_*total_orbitals_*total_orbitals_
-                 +j*total_orbitals_*total_orbitals_
-                 +k*total_orbitals_+l] = 0.0;
-          }
-        }
-      }
-    }
-  }
- 
-  //create tensors for ordering projectors
-  auto created = exatn::createTensor("Q", TENS_ELEM_TYPE, TENS_SHAPE_4INDEX); assert(created);
-  auto initialized = exatn::initTensorData("Q", orderingProjectorData); assert(initialized);
-
+  auto created = exatn::createTensor("Q", TENS_ELEM_TYPE, exatn::TensorShape{total_orbitals_, total_orbitals_, total_orbitals_, total_orbitals_}); assert(created);
+  auto success = exatn::transformTensorSync("Q", std::shared_ptr<exatn::TensorMethod>{new exatn::numerics::FunctorInitProj()});
+  
   auto appended = false;
   for (auto iter = (*ket_ansatz_).begin(); iter != (*ket_ansatz_).end(); ++iter){
     auto & network = *(iter->network);
@@ -164,7 +138,6 @@ void Simulation::constructEnergyDerivatives(){
 }
 
 void Simulation::initWavefunctionAnsatz(){
-  const auto TENS_ELEM_TYPE = exatn::TensorElementType::REAL64;
   // normalize wavefunction ansatz
   auto success = exatn::balanceNormalizeNorm2Sync(*ket_ansatz_,1.0,1.0,true);
 }
