75278f48e6ad02fbd5efd36f5f2ba9bb177be849
Elvis Maradzike <maradzikee@ornl.gov>
2021-12-17 14:10:54 -0500

    Cleans up syntax for single root calculation
    Removes unneeded solution vectors

--- tests/h4_by_onr/h4.cpp
+++ tests/h4_by_onr/h4.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv){
   const int max_bond_dim = std::min(static_cast<int>(std::pow(2,num_spin_sites/2)),bond_dim_lim);
   const int arity = 2;
   const std::string tn_type = "MPS";
-  const unsigned int num_states = 3;
+  const unsigned int num_states = 1;
   const double accuracy = 1e-4;
 
   bool success = true;
@@ -40,10 +40,10 @@ int main(int argc, char** argv){
 
   {
 
-    //Read the MCVQE Hamiltonian in spin representation:
-    auto hamiltonian0 = exatn::quantum::readSpinHamiltonian("MyHamiltonian","h_spin_representation.txt",TENS_ELEM_TYPE, "OpenFermion");
+    //Read Hamiltonian in spin representation:
+    auto hamiltonian = exatn::quantum::readSpinHamiltonian("MyHamiltonian","h_spin_representation.txt",TENS_ELEM_TYPE, "OpenFermion");
 
-    hamiltonian0->printIt();
+    hamiltonian->printIt();
     
     //Configure the tensor network builder:
     auto tn_builder = exatn::getTensorNetworkBuilder(tn_type);
@@ -58,24 +58,15 @@ int main(int argc, char** argv){
 
     //Build tensor network vectors:
     auto ket_tensor = exatn::makeSharedTensor("TensorSpace",std::vector<int>(num_spin_sites,2));
+
     auto vec_net0 = exatn::makeSharedTensorNetwork("VectorNet1",ket_tensor,*tn_builder,false);
     vec_net0->markOptimizableAllTensors();
     auto vec_tns0 = exatn::makeSharedTensorExpansion("VectorTNS1",vec_net0,std::complex<double>{1.0,0.0});
-    auto vec_net1 = exatn::makeSharedTensorNetwork("VectorNet2",ket_tensor,*tn_builder,false);
-    vec_net1->markOptimizableAllTensors();
-    auto vec_tns1 = exatn::makeSharedTensorExpansion("VectorTNS2",vec_net1,std::complex<double>{1.0,0.0});
-    auto vec_net2 = exatn::makeSharedTensorNetwork("VectorNet3",ket_tensor,*tn_builder,false);
-    vec_net2->markOptimizableAllTensors();
-    auto vec_tns2 = exatn::makeSharedTensorExpansion("VectorTNS3",vec_net2,std::complex<double>{1.0,0.0});
 
     // Create and initialize tensor network vector tensors:
     if(root) std::cout << "Creating and initializing tensor network vector tensors ... ";
     success = exatn::createTensorsSync(*vec_net0,TENS_ELEM_TYPE); assert(success);
     success = exatn::initTensorsRndSync(*vec_net0); assert(success);
-    success = exatn::createTensorsSync(*vec_net1,TENS_ELEM_TYPE); assert(success);
-    success = exatn::initTensorsRndSync(*vec_net1); assert(success);
-    success = exatn::createTensorsSync(*vec_net2,TENS_ELEM_TYPE); assert(success);
-    success = exatn::initTensorsRndSync(*vec_net2); assert(success);
     if(root) std::cout << "Ok" << std::endl;
 
     //Ground and three excited states in one call:
@@ -83,17 +74,17 @@ int main(int argc, char** argv){
     exatn::TensorNetworkOptimizer::resetDebugLevel(1,0);
     vec_net0->markOptimizableAllTensors();
     success = exatn::initTensorsRndSync(*vec_tns0); assert(success);
-    exatn::TensorNetworkOptimizer optimizer3(hamiltonian0,vec_tns0,accuracy);
-    optimizer3.enableParallelization(true);
+    exatn::TensorNetworkOptimizer optimizer(hamiltonian,vec_tns0,accuracy);
+    optimizer.enableParallelization(true);
     success = exatn::sync(); assert(success);
-    bool converged = optimizer3.optimize(num_states);
+    bool converged = optimizer.optimize(num_states);
     success = exatn::sync(); assert(success);
     if(converged){
       if(root){
         std::cout << "Search succeeded:" << std::endl;
         for(unsigned int root_id = 0; root_id < num_states; ++root_id){
           std::cout << "Expectation value " << root_id << " = "
-                 << optimizer3.getExpectationValue(root_id) << std::endl;
+                 << optimizer.getExpectationValue(root_id) << std::endl;
         }
       }
     }else{
