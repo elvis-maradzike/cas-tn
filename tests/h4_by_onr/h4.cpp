24c68900e756860dca80828f5260f094f93fee57
Elvis Maradzike <maradzikee@ornl.gov>
2021-12-22 08:47:39 -0500

    This adds tidier sources plus one file containing ...
    a preoptimized contiguous tensor, file name: /tests/h4_by_onr/optimized_tensor.txt

--- tests/h4_by_onr/h4.cpp
+++ tests/h4_by_onr/h4.cpp
@@ -30,66 +30,66 @@ int main(int argc, char** argv){
   const std::string tn_type = "MPS";
   const unsigned int num_states = 1;
   const double accuracy = 1e-4;
-
   bool success = true;
   bool root = (exatn::getProcessRank() == 0);
 
   exatn::initialize();
-
   {
+  // define input tensor to make target:
+  auto target_tensor = exatn::makeSharedTensor("TargetTensor",std::vector<int>(num_spin_sites,2));
+  auto success = exatn::createTensor(target_tensor, TENS_ELEM_TYPE);
+  auto target_net = exatn::makeSharedTensorNetwork("TargetNet");
+  target_net->appendTensor(1,target_tensor,{});
+  target_net->markOptimizableAllTensors();
+  auto target = exatn::makeSharedTensorExpansion();
+  target->appendComponent(target_net,{1.0,0.0});
+  target->rename("TargetExpansion");
+  //target->printIt();
+  target->markOptimizableAllTensors();
 
-    // define tensor expanstion explicitly
-    const unsigned int num_states = 1;
-    const double accuracy = 1e-4;
-    const auto TENS_ELEM_TYPE = exatn::TensorElementType::COMPLEX64;
-    auto c = exatn::makeSharedTensor("C", exatn::TensorShape{2,2,2,2,2,2,2,2});
-    auto success = exatn::createTensor(c, TENS_ELEM_TYPE);
-    auto net = exatn::makeSharedTensorNetwork("Net");
-    net->appendTensor(1,c,{});
-    auto ansatz = exatn::makeSharedTensorExpansion();
-    ansatz->appendComponent(net,{1.0,0.0});
-    ansatz->rename("Ansatz");
-    ansatz->printIt();
-    ansatz->markOptimizableAllTensors();
+  // configure the tensor network builder:
+  auto tn_builder = exatn::getTensorNetworkBuilder(tn_type);
+  if(tn_type == "MPS"){
+   success = tn_builder->setParameter("max_bond_dim",max_bond_dim); assert(success);
+  }else if(tn_type == "TTN"){
+   success = tn_builder->setParameter("max_bond_dim",max_bond_dim); assert(success);
+   success = tn_builder->setParameter("arity",arity); assert(success);
+  }else{
+   assert(false);
+  }
+  
+  // output tensor
+  auto approximant_tensor = exatn::makeSharedTensor("TensorSpace",std::vector<int>(num_spin_sites,2));
+  success = exatn::createTensor(approximant_tensor, TENS_ELEM_TYPE);
+  //approximant_tensor->printIt();
 
-    //Read Hamiltonian in spin representation:
-    auto hamiltonian = exatn::quantum::readSpinHamiltonian("MyHamiltonian","h_spin_representation.txt",TENS_ELEM_TYPE, "OpenFermion");
+  // tensor network 
+  auto approximant_net = exatn::makeSharedTensorNetwork("ApproximantNetwork",approximant_tensor,*tn_builder,false);
+  approximant_net->markOptimizableAllTensors();
 
-    hamiltonian->printIt();
-    
-    //Configure the tensor network builder:
-    auto tn_builder = exatn::getTensorNetworkBuilder(tn_type);
-    if(tn_type == "MPS"){
-     success = tn_builder->setParameter("max_bond_dim",max_bond_dim); assert(success);
-    }else if(tn_type == "TTN"){
-     success = tn_builder->setParameter("max_bond_dim",max_bond_dim); assert(success);
-     success = tn_builder->setParameter("arity",arity); assert(success);
-    }else{
-     assert(false);
-    }
+  // tensor network expansion
+  auto approximant = exatn::makeSharedTensorExpansion();
+  approximant->appendComponent(approximant_net,{1.0,0.0});
+  approximant->rename("Approximant");
+  
+  // read in Hamiltonian
+  auto hamiltonian = exatn::quantum::readSpinHamiltonian("MyHamiltonian","h_spin_representation.txt",TENS_ELEM_TYPE, "OpenFermion");
 
-    //Build tensor network vectors:
-    auto ket_tensor = exatn::makeSharedTensor("TensorSpace",std::vector<int>(num_spin_sites,2));
-    auto vec_net0 = exatn::makeSharedTensorNetwork("VectorNet1",ket_tensor,*tn_builder,false);
-    vec_net0->markOptimizableAllTensors();
-    auto vec_tns0 = exatn::makeSharedTensorExpansion("VectorTNS1",vec_net0,std::complex<double>{1.0,0.0});
+  // create and initialize tensor network 
+  if(root) std::cout << "Creating and initializing tensor network vector tensors ... " << std::endl;
+  success = exatn::createTensorsSync(*approximant_net,TENS_ELEM_TYPE); assert(success);
+  success = exatn::initTensorsRndSync(*approximant_net); assert(success);
+  success = exatn::initTensorFile("TargetTensor", "optimized_tensor.txt"); assert(success);
+  if(root) std::cout << "Ok" << std::endl;
 
-   // Create and initialize tensor network vector tensors:
-    if(root) std::cout << "Creating and initializing tensor network vector tensors ... " << std::endl;
-    success = exatn::createTensorsSync(*net,TENS_ELEM_TYPE); assert(success);
-    success = exatn::initTensorFile("C", "optimized_ansatz.txt"); assert(success);
-    success = exatn::createTensorsSync(*vec_net0,TENS_ELEM_TYPE); assert(success);
-    success = exatn::initTensorsRndSync(*vec_net0); assert(success);
-    if(root) std::cout << "Ok" << std::endl;
-
-    //run reconstruction
-    vec_tns0->conjugate();
-    exatn::TensorNetworkReconstructor reconstructor(ansatz,vec_tns0,1e-5);
-    reconstructor.resetDebugLevel(2,0);
-    success = exatn::sync(); assert(success);
-    double residual_norm, fidelity;
-    bool reconstructed = reconstructor.reconstruct(&residual_norm,&fidelity,true);
-    success = exatn::sync(); assert(success);
+  // run reconstruction
+  approximant->conjugate();
+  exatn::TensorNetworkReconstructor reconstructor(target,approximant,1e-7);
+  reconstructor.resetDebugLevel(2,0);
+  success = exatn::sync(); assert(success);
+  double residual_norm, fidelity;
+  bool reconstructed = reconstructor.reconstruct(&residual_norm,&fidelity,true);
+  success = exatn::sync(); assert(success);
     if(reconstructed){
       std::cout << "Reconstruction succeeded: Residual norm = " << residual_norm
               << "; Fidelity = " << fidelity << std::endl;
@@ -97,12 +97,13 @@ int main(int argc, char** argv){
       std::cout << "Reconstruction failed!" << std::endl; assert(false);
     }
 
-    
-    //Ground state:
+
+    approximant->conjugate();
+   
+    // ground state:
     if(root) std::cout << "Ground state search for the original Hamiltonian:" << std::endl;
-    exatn::TensorNetworkOptimizer::resetDebugLevel(2,0);
-    vec_tns0->conjugate();
-    exatn::TensorNetworkOptimizer optimizer(hamiltonian,vec_tns0,accuracy);
+    exatn::TensorNetworkOptimizer::resetDebugLevel(1,0);
+    exatn::TensorNetworkOptimizer optimizer(hamiltonian,approximant,accuracy);
     optimizer.enableParallelization(true);
     success = exatn::sync(); assert(success);
     bool converged = optimizer.optimize(num_states);
--- tests/h4_by_onr_contiguous_ansatz/h4.cpp
+++ tests/h4_by_onr_contiguous_ansatz/h4.cpp
@@ -25,61 +25,70 @@ int main(int argc, char** argv){
   exatn::initialize(exatn_parameters, "lazy-dag-executor");
 #endif
 
+  // parameters
+  const unsigned int num_states = 1;
+  const double accuracy = 1e-4;
+  const auto TENS_ELEM_TYPE = exatn::TensorElementType::COMPLEX64;
+
   exatn::initialize();
   {
+  // define tensor expansion ansatz:
+  auto c = exatn::makeSharedTensor("C", exatn::TensorShape{2,2,2,2,2,2,2,2});
+  auto success = exatn::createTensor(c, TENS_ELEM_TYPE);
+  auto net = exatn::makeSharedTensorNetwork("Net");
+  net->appendTensor(1,c,{});
+  net->markOptimizableAllTensors();
+  auto ansatz = exatn::makeSharedTensorExpansion();
+  ansatz->appendComponent(net,{1.0,0.0});
+  ansatz->rename("Ansatz");
+  //ansatz->printIt();
+  ansatz->markOptimizableAllTensors();
+  
+  bool root = (exatn::getProcessRank() == 0);
+  auto solution_vector = exatn::makeSharedTensorExpansion("SolutionVector",net,std::complex<double>{1.0,0.0});
+  
+  // read Hamiltonian in spin representation:
+  auto hamiltonian = exatn::quantum::readSpinHamiltonian("MyHamiltonian","h_spin_representation.txt",TENS_ELEM_TYPE, "OpenFermion");
+  //hamiltonian->printIt();
+  
+  // create and initialize tensor network vector tensors:
+  if(root) std::cout << "Creating and initializing tensor network vector tensors ... ";
+  //success = exatn::createTensorsSync(*net,TENS_ELEM_TYPE); assert(success);
+  //success = exatn::initTensorsRndSync(*net); assert(success);
+  success = exatn::initTensorRndSync("C"); assert(success);
+  if(root) std::cout << "Ok" << std::endl;
 
-    // define tensor expanstion explicitly
-    const unsigned int num_states = 1;
-    const double accuracy = 1e-4;
-    const auto TENS_ELEM_TYPE = exatn::TensorElementType::COMPLEX64;
-    auto c = exatn::makeSharedTensor("C", exatn::TensorShape{2,2,2,2,2,2,2,2});
-    auto success = exatn::createTensor(c, TENS_ELEM_TYPE);
-    auto net = exatn::makeSharedTensorNetwork("Net");
-    net->appendTensor(1,c,{});
-    auto ansatz = exatn::makeSharedTensorExpansion();
-    ansatz->appendComponent(net,{1.0,0.0});
-    ansatz->rename("Ansatz");
-    ansatz->printIt();
-    ansatz->markOptimizableAllTensors();
-    
-    bool root = (exatn::getProcessRank() == 0);
-    
-    //Read Hamiltonian in spin representation:
-    auto hamiltonian = exatn::quantum::readSpinHamiltonian("MyHamiltonian","h_spin_representation.txt",TENS_ELEM_TYPE, "OpenFermion");
-    hamiltonian->printIt();
-    
-    // Create and initialize tensor network vector tensors:
-    if(root) std::cout << "Creating and initializing tensor network vector tensors ... ";
-    success = exatn::createTensorsSync(*net,TENS_ELEM_TYPE); assert(success);
-    success = exatn::initTensorsRndSync(*net); assert(success);
-    if(root) std::cout << "Ok" << std::endl;
-
-    //Ground state:
-    if(root) std::cout << "Ground and excited states search for the original Hamiltonian:" << std::endl;
-    exatn::TensorNetworkOptimizer::resetDebugLevel(1,0);
-    net->markOptimizableAllTensors();
-    success = exatn::initTensorsRndSync(*ansatz); assert(success);
-    exatn::TensorNetworkOptimizer optimizer(hamiltonian,ansatz,accuracy);
-    optimizer.enableParallelization(true);
-    success = exatn::sync(); assert(success);
-    bool converged = optimizer.optimize(num_states);
-    success = exatn::sync(); assert(success);
-    if(converged){
-      if(root){
-        std::cout << "Search succeeded:" << std::endl;
-        for(unsigned int root_id = 0; root_id < num_states; ++root_id){
-          std::cout << "Expectation value " << root_id << " = "
-                 << optimizer.getExpectationValue(root_id) << std::endl;
+  // ground state:
+  if(root) std::cout << "Ground and excited states search for the original Hamiltonian:" << std::endl;
+  exatn::TensorNetworkOptimizer::resetDebugLevel(2,0);
+  exatn::TensorNetworkOptimizer optimizer(hamiltonian,ansatz,accuracy);
+  optimizer.enableParallelization(true);
+  success = exatn::sync(); assert(success);
+  bool converged = optimizer.optimize(num_states);
+  success = exatn::sync(); assert(success);
+  if(converged){
+    if(root){
+      std::cout << "Search succeeded:" << std::endl;
+      for(unsigned int root_id = 0; root_id < num_states; ++root_id){
+        std::cout << "Expectation value " << root_id << " = "
+               << optimizer.getExpectationValue(root_id) << std::endl;
+      }
+    }
+    for(auto network = ansatz->cbegin(); network != ansatz->cend(); ++network){
+      for(auto tensor_conn = network->network->begin(); tensor_conn != network->network->end(); ++tensor_conn){
+        const auto & tensor = tensor_conn->second;
+        if (tensor.isOptimizable()){
+          std::cout << "tensor name " << tensor.getName() << std::endl;
+          success = exatn::printTensorFileSync(tensor.getName(),"optimized_tensor.txt"); assert(success);
         }
       }
-      success = exatn::printTensorFileSync("C","ansatz_optimized.txt"); assert(success);
-    }else{
-      if(root) std::cout << "Search failed!" << std::endl;
-      assert(false);
     }
-    
+      
+  }else{
+    if(root) std::cout << "Search failed!" << std::endl;
+    assert(false);
   }
-
+  }
   exatn::finalize();
 
 #ifdef MPI_ENABLED
@@ -90,4 +99,3 @@ int main(int argc, char** argv){
   return 0;
 
 }
-
